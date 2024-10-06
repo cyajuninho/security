fff0f2b40c641dd09b6134242f20bec203555d5a
junior <junior@fjsistemas.com.br>
2024-10-06 12:51:16 -0300

    feat: entities

---
+++ src/main/java/br/com/cya/security/model/Role.java
@@ -0,0 +1,30 @@
+package br.com.cya.security.model;
+
+import br.com.cya.security.security.RoleName;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="roles")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
+}
