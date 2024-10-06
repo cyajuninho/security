47cb66d6ccc8f5303ecfecddfeba198819055c14
junior <junior@fjsistemas.com.br>
2024-10-06 16:51:24 -0300

    feat: adding securityFilterChain and other routes to admin and simple user

--- src/main/java/br/com/cya/security/config/SecurityConfig.java
+++ src/main/java/br/com/cya/security/config/SecurityConfig.java
@@ -1,15 +1,73 @@
 package br.com.cya.security.config;
 
+import br.com.cya.security.auth.UserAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserAuthenticationFilter userAuthenticationFilter;
+
+    @Autowired
+    public SecurityConfig (UserAuthenticationFilter userAuthenticationFilter) {
+        this.userAuthenticationFilter = userAuthenticationFilter;
+    }
 
     // Endpoints que não necessitam de autenticação
     public static final String[] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
             "/users/login",
             "/users"
     };
+
+    // Endpoints que necessitam de autenticação
+    public static final String[] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {
+            "/users/homepage"
+    };
+
+    // Endpoints para o perfil de usuário
+    public static final String[] ENDPOINTS_USER = {
+            "/users/perfil"
+    };
+
+    // Endpoints para o perfil de administrador
+    public static final String[] ENDPOINTS_ADMIN = {
+            "/users/admin"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
+                        .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_REQUIRED).authenticated()
+                        .requestMatchers(ENDPOINTS_USER).hasRole("USER")
+                        .requestMatchers(ENDPOINTS_ADMIN).hasRole("ADMIN")
+                        .anyRequest().denyAll()
+                )
+                .addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable); // Desabilita o CSRF
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
