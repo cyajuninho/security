f05961897140afab5981cc9db2ea32a9f1a16cd8
junior <junior@fjsistemas.com.br>
2024-10-06 13:03:15 -0300

    feat: userdetails by spring

---
+++ src/main/java/br/com/cya/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package br.com.cya.security.service;
+
+import br.com.cya.security.model.User;
+import br.com.cya.security.repository.UserRepository;
+import br.com.cya.security.security.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+        return new UserDetailsImpl(user);
+    }
+
+}
